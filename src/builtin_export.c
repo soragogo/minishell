edea038f5c2446ed9d5c53fef0c1415bd31c5be9
Mayu Yamada <mr.obake0208@yahoo.com>
2023-08-29 21:06:37 +0900

    tyottodake syusei

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -1,24 +1,40 @@
 #include "../includes/minishell.h"
 
-void ft_export(t_env **map, char *arg)
+void ft_export(t_env **map, char **arg)
 {
 	char *name;
 	char *value;
 
-	name = get_env_name(name, arg);
-	value = get_env_value(value, arg);
+	name = get_env_name(name, arg[1]);
+	value = get_env_value(value, arg[1]);
 	if(set_env(map, name, value) == -1)
 		printf("error");
 	// set_env(&map, "TEST_VARIABLE", "hello");
 }
 
-// int main(){
-// 	t_env *map = map_new();
-//     envmap_init(&map);
-// 	char *arg = "TEST_VARIABLE=hello";
-// 	ft_env(&map, arg);
+
+// void test_ft_export(t_env **map, char **arg) {
+//     printf("Setting environment variable: %s\n", *arg);
+//     ft_export(map, arg);
+//     printf("Environment variable set.\n\n");
+// }
+
+// int main() {
+//     // テストケースを用意して実行
+//     t_env *map = map_new();
+//     envmap_init(&map);  // mapの初期化は実際の状況に合わせて修正
+    
+
+//     // char *test1 = "MY_VARIABLE=value123";
+// 	char *test1[] = {"export", "MY_VARIABLE=value123"};
+// 	ft_env(&map, *test1);
+//     test_ft_export(&map, test1);
+
+//     char *test2[] = {"export", "ANOTHER_VARIABLE=hello_world"};
+//     test_ft_export(&map, test2);
+	
 // 	printf("----------------------------------------\n");
-// 	ft_export(&map, arg);
-// 	ft_env(&map, arg);
-// 	return 0;
+// 	ft_env(&map, *test1);
+
+//     return 0;
 // }
--- src/builtin_unset.c
+++ src/builtin_unset.c
@@ -1,19 +1,29 @@
 #include "../includes/minishell.h"
 
 //後で綺麗にします
-void ft_unset(t_env **env_head, char *name)
+void ft_unset(t_env **env_head, char **arg)
 {
-	env_unset(env_head, name);
+	env_unset(env_head, arg[1]);
 }
 
-// int main (){
-// 	t_env *map = map_new();
-//     envmap_init(&map);
-// 	set_env(&map, "TEST_VARIABLE", "hello");
-// 	char *arg = "TEST_VARIABLE";
-// 	ft_env(&map, arg);
+// void test_ft_unset(t_env **env_head, char **name) {
+// 	printf("Unsetting environment variable: %s\n", name[1]);
+// 	ft_unset(env_head, name);
+// 	printf("Environment variable unset.\n\n");
+// }
+
+// int main() {
+// 	// テストケースを用意して実行
+// 	t_env *env = map_new();
+//     envmap_init(&env);
+
+// 	char *test1[] = {"unset", "TEST_VARIABLE1"};
+// 	test_ft_unset(&env, test1);
+// 	set_env(&env, "TEST_VARIABLE2", "hello");
+// 	ft_env(&env, "TEST_VARIABLE2");
 // 	printf("----------------------------------------\n");
-// 	ft_unset(&map, arg);
-// 	ft_env(&map, arg);
+// 	char *test2[] = {"unset", "TEST_VARIABLE2"};
+// 	test_ft_unset(&env, test2);
+// 	ft_env(&env, "TEST_VARIABLE2");
 // 	return 0;
 // }
